b9e1ee4be6e161fc112390308a7e811adbaa0ad2
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-09-23 10:31:02 -0500

    refactor: Actualización de la configuracion del servidor web
    Se actualizó la configuración anterior del servidor web para evitar conflictos con la nueva infraestructura.

--- iac/web-server.tf
+++ iac/web-server.tf
@@ -1,8 +1,8 @@
-resource "docker_container" "nginx"{
-    name = "nginx-iac-lab02"
-    image = docker_image.nginx.image_id
-    ports{
-        external =  var.web_server_port[terraform.workspace]
-        internal =  80
-    }
-}
+# resource "docker_container" "nginx" {
+# name  = "nginx-iac-lab02"
+#  image = docker_image.nginx.image_id
+#  ports {
+#    external = var.web_server_port[terraform.workspace]
+#    internal = 80
+#  }
+#}
