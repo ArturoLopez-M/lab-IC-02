191b85d328f67b370d30767fc19d4550b032b0c7
Arturo Lopez <jlopezm8@upao.edu.pe>
2026-09-23 10:27:39 -0500

    refactor: Configuración de puertos por entorno
    Se configuró los puertos para los entornos "dev" y "qa" mediante variables.

--- iac/terraform.tfvars
+++ iac/terraform.tfvars
@@ -1,6 +1,15 @@
 web_server_port = {
-    default = 3000
-    prod = 8080
-    qa = 5001
-    dev = 4001
+  default = 3000
+  prod    = 8080
+  qa      = 5001
+  dev     = 4001
+}
+frontend_port = {
+  dev = 4001
+  qa  = 5001
+}
+
+backend_port = {
+  dev = 4002
+  qa  = 5002
 }
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,4 +1,12 @@
-variable "web_server_port"{
-    description = "Puertos del frontend según el entorno"
-    type = map(number)
+variable "web_server_port" {
+  description = "Puertos del servidor según el entorno"
+  type        = map(number)
+}
+
+variable "frontend_port" {
+  type = map(number)
+}
+
+variable "backend_port" {
+  type = map(number)
 }
